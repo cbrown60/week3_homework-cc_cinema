98908997ccfe5d9948a67932caf8dba7a289f6c1
Chris <cbrown605@googlemail.com>
2017-02-17 11:31:00 +0000

    added INT to console first table creation successful

--- db/cinema.sql
+++ db/cinema.sql
@@ -3,20 +3,21 @@ DROP TABLE films;
 DROP TABLE tickets; 
 
 CREATE TABLE customers(
-id SERIAL4 PRIMARY KEY ,
+id SERIAL4 PRIMARY KEY,
 name VARCHAR(255),
 age INT2, 
-bank INT4);
+bank INT4
+);
 
 
 CREATE TABLE films(
 id SERIAL4 PRIMARY KEY,
 title VARCHAR(255), 
-age_rating INT2;
+age_rating INT2);
 
 CREATE TABLE tickets(
 id SERIAL4 PRIMARY KEY,
-price INT4
-customer_id REFERENCES customers(id) ON DELETE CASCADE,
-film_id REFERENCES films(id) ON DELETE CASCADE
+price INT4,
+customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+film_id INT4 REFERENCES films(id) ON DELETE CASCADE
 );
